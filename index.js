0d9ee630f6d0e90a7528092b0fe6794ffcb93797
ismayilovcode <ismayilovterlan508@gmail.com>
2024-02-28 21:53:24 +0400

    added project

---
+++ index.js
@@ -0,0 +1,39 @@
+// version 1
+
+// let x = +prompt("x:");
+// let y = +prompt("y:");
+
+// if (x == 50 || y == 50 || x + y == 50) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+// version 1.1
+
+// let x = +prompt("x:");
+// let y = +prompt("y:");
+
+// console.log (x == 50 || y == 50 || x + y == 50  ? true : false)
+
+// version 2
+
+// let x = +prompt("x:");
+// let y = +prompt("y:");
+
+// if (x == 50) {
+//   console.log(true);
+// } else if (y == 50) {
+//   console.log(true);
+// } else if (x + y == 50) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// version 2.1
+
+// let x = +prompt("x:");
+// let y = +prompt("y:");
+
+// console.log (x == 50 ? true : y == 50 ? true : x + y == 50)
